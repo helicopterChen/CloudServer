d0c86c57d511c19dd59faada3a093de37456d532
helicopterchen <helicopter@mail.com>
2018-10-22 14:03:19 +0800

    test

--- CloudScript.js
+++ CloudScript.js
@@ -6,3 +6,20 @@ handlers.TestScript = function (args) {
     var tUserData = server.GetUserData(request);
 	return {result: tUserData}
 };
+
+
+handlers.AddEmailToPlayer = function (args) {
+	var request = 
+    {
+        PlayFabId :currentPlayerId,
+	    "Data": 
+	    {
+		    "Emails":
+		    [
+		    	{"X":"T1"},
+		    	{"Z":"T2"}
+		    ]
+		}
+    };
+    server.UpdateUserData(request);  
+};
