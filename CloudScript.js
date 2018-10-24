38993abc497b364f3d60629c81f7d1e5e027100c
helicopterchen <helicopter@mail.com>
2018-10-24 16:36:35 +0800

    1

--- CloudScript.js
+++ CloudScript.js
@@ -34,6 +34,6 @@ handlers.PlayerReadMail = function(args) {
         ]
     };
 	var tResult = server.GetUserData( request );
-	var tData = JSON.parse(tResult.data.Data.emails.Value);
-    return tData;
+	//var tData = JSON.parse(tResult.data.Data.emails.Value);
+    return tResult.data;
 }
